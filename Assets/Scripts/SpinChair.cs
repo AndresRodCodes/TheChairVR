979c98d23d2cd992a023455de30c95d6a26b22e5
unknown <andres6459@gmail.com>
2021-04-27 19:08:01 -0700

    Able to spin chair with the controller grip button.

--- Assets/Scripts/SpinChair.cs
+++ Assets/Scripts/SpinChair.cs
@@ -6,6 +6,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class SpinChair : MonoBehaviour
 {
     public GameObject ObjectToSpin;
+    public float speed;
 
     private UnityEngine.XR.InputDevice leftController;
     private UnityEngine.XR.InputDevice rightController;
@@ -28,14 +29,22 @@ public class SpinChair : MonoBehaviour
         var gameContorllers = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.Controller, gameContorllers);
 
-        leftController = gameContorllers[0];
-        rightController = gameContorllers[1];
+        if (gameContorllers.Count > 0)
+        {
+            leftController = gameContorllers[0];
+            rightController = gameContorllers[1];
+        }
     }
 
     void SpinGameObject()
     {
         float gripValue;
+        // Get controller grip value
         rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out gripValue);
         Debug.Log("Grip Value: " + gripValue);
+
+        // Rotate Chair with grip value
+        //ObjectToSpin.transform.rotation = Quaternion.Euler(0, gripValue * 360, 0);
+        ObjectToSpin.transform.Rotate(0, (float)(gripValue * speed * Time.deltaTime), 0);
     }
 }
