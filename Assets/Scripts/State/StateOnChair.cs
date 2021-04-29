7c17c5ab91a6188469b588cede24020ae3cf24ee
unknown <andres6459@gmail.com>
2021-04-28 17:26:07 -0700

    Added skeleton for player state.

---
+++ Assets/Scripts/State/StateOnChair.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateOnChair : State
+{
+    public override IEnumerator OnChair()
+    {
+        return base.OnChair();
+    }
+}
